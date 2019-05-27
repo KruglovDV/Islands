38f2880cb539e833d1a9639a13b3e59742c0368c
Dmitry Kruglov <kruglov081289@mail.ru>
2019-05-27 20:41:46 +0300

    Feature: add core rules

---
+++ lib/islands_engine/application.ex
@@ -0,0 +1,15 @@
+defmodule IslandsEngine.Application do
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: IslandsEngine.Worker.start_link(arg)
+      # {IslandsEngine.Worker, arg}
+    ]
+
+    opts = [strategy: :one_for_one, name: IslandsEngine.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
