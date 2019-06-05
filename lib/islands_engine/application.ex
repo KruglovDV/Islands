f50b353373ca7b4d69df7cfd9cf2433dd608119a
Dmitry Kruglov <kruglov081289@mail.ru>
2019-06-05 19:46:13 +0300

    Fix: supervisor children

--- lib/islands_engine/application.ex
+++ lib/islands_engine/application.ex
@@ -5,7 +5,7 @@ defmodule IslandsEngine.Application do
 
   def start(_type, _args) do
     children = [
-      via = Game.via_tuple("Lena"):sys.get_state(via)  {Registry, keys: :unique, name: Registry.Game}
+      {Registry, keys: :unique, name: Registry.Game}
     ]
 
     opts = [strategy: :one_for_one, name: IslandsEngine.Supervisor]
